0e232a5bf69f75da6c80c71bccd35fcf44247260
Erin Green <erin.y.green@gmail.com>
2019-05-20 20:04:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -90,6 +90,11 @@ class Meal {
     })
   }
 
+
+    static byPrice() {
+      return store.meals.sort((a, b) => a.price < b.price);
+    }
+
   // static byPrice() {
   //   return store.meals.sort((a, b) => a.price - b.price;)
   // }
